f5772403055fd791079bfc6f5863511cf8bbfaf1
ArianneAlonso <ariannealonso8@gmail.com>
2024-09-05 16:41:16 -0300

    se modifico app.js

--- backend-session/app.js
+++ backend-session/app.js
@@ -1,40 +1,25 @@
-
 import cors from 'cors';
 import express from 'express';
 import session from 'express-session';
 import morgan from 'morgan';
-import path from 'path';
-
 import { conectar } from './db/database.js';
 import router from './routers/routers.js';
 
 const app = express();
-const PORT = process.env.PORT || 4000;
 
-const __dirname = path.resolve();
+//puerto
+const PORT = process.env.PORT || 3000;
 
-// Middlewares 
-app.use(cors({ // Permitir solicitudes desde el front-end
-    origin: [
-        'http://localhost:5500',
-        'http://localhost:3000',
-    ],
-    methods: ['GET', 'POST', 'PUT', 'DELETE'],
-    credentials: true // Habilitar envío de cookies
-}));
+//bd
+conectar();
+
+// middlewares
 app.use(morgan('dev'));
+app.use(cors());
 app.use(express.json());
-app.use(express.static(path.join(__dirname, 'public')));
-app.use(session({
-    secret: 'mi_secreto',
-    resave: false,
-    saveUninitialized: true,
-    cookie: { 
-        secure: false, // true solo si usas HTTPS
-        httpOnly: true, // evita acceso a cookie desde JavaScript del cliente
-        // sameSite: 'lax' // permite envío de cookies en navegadores modernos
-    }
-}));
-app.use(router)
 
+// rutas
+app.use('/', router);
+
+// escucha del server
 app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}/`));
