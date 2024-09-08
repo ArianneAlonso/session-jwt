5e132ef1113eb7a26061bedcb958be6507eef18c
arianne <ariannealonso8@gmail.com>
2024-09-08 11:33:31 -0300

    se importò session

--- backend-session/routers/routers.js
+++ backend-session/routers/routers.js
@@ -1,11 +1,24 @@
-import {Router} from "express";
-import { closeSession, loginSession, account, register } from "../controllers/controllers.js";
+import { Router } from "express";
 import session from "express-session";
+import { closeSession, loginSession, account, register } from "../controllers/controllers.js";
 
-const router = Router(); 
-router.post('/logout', closeSession );
-router.get('/session', account );
-router.post('/login', loginSession );
+const router = Router();
+
+// middleware de session
+router.use(session({
+    secret: 'mi_secreto',
+    resave: false,
+    saveUninitialized: true,
+    cookie: { 
+        secure: false, // true solo si usas HTTPS
+        httpOnly: true, // evita acceso a cookie desde JavaScript del cliente
+        // sameSite: 'lax' // permite envío de cookies en navegadores modernos
+    }
+}));
+
+router.post('/logout', closeSession);
+router.get('/session', account);
+router.post('/login', loginSession);
 router.post('/register', register);
 
-export default router
+export default router;
